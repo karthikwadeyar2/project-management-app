eadda3598aabe0fbbf8390e64212b06e427010ce
karthikwadeyar2 <karthik.wadeyar2@cognizant.com>
2024-04-22 15:11:48 +0530

    Created Button component for code reusability

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSideBar.jsx";
 
 function App() {
+  const [] = useState({})
   return (
     <main className="h-screen my-8 flex gap-8  ">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
