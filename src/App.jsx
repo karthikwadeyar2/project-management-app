12d98a8b503dee4cb7804762033fec5cc492c852
karthikwadeyar2 <karthik.wadeyar2@cognizant.com>
2024-04-23 12:20:33 +0530

    Created component Selected Project to show details and handled deletion of the Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
-import ProjectsSidebar from "./components/ProjectsSideBar.jsx";
+import ProjectsSidebar from "./components/ProjectsSidebar.jsx"
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   })
+
+  function handleSelectProject(id){
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      }
+    })
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -42,9 +52,19 @@ function App() {
     });
   }
 
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId)
+      }
+    })
+  }
 
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
 
 
   if(projectsState.selectedProjectId === null) {
@@ -55,7 +75,11 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8  ">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects} />
+      <ProjectsSidebar 
+        onStartAddProject={handleStartAddProject} 
+        projects={projectsState.projects} 
+        onSelectProject={handleSelectProject}
+      />
       {content}
     </main>
   );
