bc34cc7a14c50844bed24553851d659e44a51448
karthikwadeyar2 <karthik.wadeyar2@cognizant.com>
2024-04-23 10:22:23 +0530

    Created a Modal component to show error message

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({onAdd, onCancel}) {
+  const modal = useRef()
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +14,12 @@ export default function NewProject({onAdd}) {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation...
+    if(enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredDueDate.trim() === ''){
+      // show the error modal
+      modal.current.open()
+      return
+    }
+
     onAdd({
         title: enteredTitle,
         description: enteredDescription,
@@ -21,10 +29,16 @@ export default function NewProject({onAdd}) {
   }
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Close">
+      <h2 className='text-xl font-bold text-stone-700 my-4'>Invalid Input</h2>
+      <p className='text-stone-600 mb-4'>Oops.. looks like you forgot to enter a value</p>
+      <p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4 ">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">
+          <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>
             Cancel
           </button>
         </li>
@@ -41,5 +55,6 @@ export default function NewProject({onAdd}) {
         <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
+    </>
   );
 }
