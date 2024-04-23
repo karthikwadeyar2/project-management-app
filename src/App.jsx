d4df80ac2337f735bf6f41675311da06aa78102c
karthikwadeyar2 <karthik.wadeyar2@cognizant.com>
2024-04-23 15:27:35 +0530

    Created Tasks component to manage tasks for the project

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,33 @@ import SelectedProject from "./components/SelectedProject.jsx";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   })
+
+  function handleAddTask(text) {
+    setProjectsState((prevState) => {
+      const taskId = Math.random()
+      const newTask = {
+        text: text,
+        projectId: prevState.selectedProjectId,
+        id: taskId
+      }
+      return {
+        ...prevState,
+        tasks: [newTask,...prevState.tasks]
+      }
+    })
+  }
+
+  function handleDeleteTask(/*taskId*/) {
+    // setProjectsState(prevState => {
+    //   return {
+    //     ...prevState,
+    //     tasks: prevState.tasks.filter(task => task.id !== taskId)
+    //   }
+    // })
+  }
 
   function handleSelectProject(id){
     setProjectsState(prevState => {
@@ -64,7 +89,15 @@ function App() {
 
   const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject} 
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+  )
 
 
   if(projectsState.selectedProjectId === null) {
